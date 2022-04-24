0599a6eafc53eca5225cbbb0343e5864e4c4e9c8
ajormoses <ajormoses@gmail.com>
2022-04-24 19:56:28 +0100

    Project completed

---
+++ main.js
@@ -0,0 +1,34 @@
+const email = document.querySelector("#email");
+const submit = document.querySelectorAll(".submit");
+var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+submit.forEach((submit) => {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (email.value.trim() == "") {
+      error(email, "Please provide a vaild email address");
+    } else {
+      success(email);
+    }
+
+    if (email.value.match(pattern)) {
+      success(email);
+    } else {
+      error(email, "Please provide a vaild email address");
+    }
+  });
+});
+
+function error(element, msg) {
+  element.style.border = "1px solid hsl(354, 100%, 66%)";
+  const parent = element.parentElement;
+  const p = parent.querySelector(".text");
+  p.style.display = "block";
+  p.innerHTML = msg;
+  p.style.color = "hsl(354, 100%, 66%)";
+}
+
+function success(element) {
+  element.style.border = "1px solid hsl(223, 87%, 63%)";
+}
